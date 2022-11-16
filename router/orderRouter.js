a936e66d9f05c96cbee77ec6fe56295904777258
sammyalwayscode <olorundasamuel2@gmail.com>
2022-11-16 16:07:37 +0100

    bag

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
-const { createOrder, getOrders } = require("../controller/orderController");
+const {
+  createOrder,
+  getOrders,
+  createPreOrder,
+  getPreOrders,
+} = require("../controller/orderController");
 const router = Router();
 
 router.route("/").get(getOrders);
 router.route("/newOrder").post(createOrder);
+router.route("/preOrder").post(createPreOrder);
+router.route("/preOrders").get(getPreOrders);
 
 module.exports = router;
